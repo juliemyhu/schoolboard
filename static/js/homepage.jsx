5017700b744776722a50841c60930139a6807de8
Julie Hu <jhu21495@gmail.com>
2020-06-09 00:57:58 +0000

    submit handler makes fetch request to API

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,31 +1,50 @@
 const MY_API_KEY ='free_d6e794d268065566fd05b280ee'
 
-
-
 class ProgramForm extends React.Component {
-    constructor() {
-        super();
-        this.state={value: ''};
+    constructor(props) {
+        super(props);
+        this.state={
+            value: '',
+            program: '',
+            college: '',
+            cohort: '',
+            link: ''
+    };
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleChangeProgram = this.handleChangeProgram.bind(this);
+        this.handleChangeCollege= this.handleChangeCollege.bind(this);
+        this.handleChangeCohort= this.handleChangeCohort.bind(this);
+        this.handleChangeLink= this.handleChangeLink.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
+    handleChangeProgram(event) {
+        this.setState({program: event.target.value});
+    }
+    handleChangeCollege(event) {
+        this.setState({college: event.target.value});
+    }
+    handleChangeCohort(event) {
+        this.setState({cohort: event.target.value});
+    }
+    handleChangeLink(event) {
+        this.setState({link: event.target.value});
     }
 
     handleSubmit(event) {
         alert('the program submit button was clicked');
+        console.log(this.state)
+        console.log('college:', this.state.college)
         event.preventDefault();
+
+        const college = this.state.college
+        const URL = 'https://api.collegeai.com/v1/api/college/info?api_key=free_d6e794d268065566fd05b280ee&college_names=' + college + '&info_ids=city%2Clocation_lat%2Clocation_long%2C'
         
-    //     const data = new FormData(event.target);
-    //     console.log(data);
-    //     fetch("https://api.collegeai.com/v1/api/college/info?api_key=free_d6e794d268065566fd05b280ee&college_names=${college}&info_ids=city%2Clocation_lat%2Clocation_long%2C")
-    //     .then(r => r.json())
-    //     .then(response => {
-    //         console.log(response)
-    //     })
+        fetch(URL)
+        .then(r => r.json())
+        .then(response => {
+            console.log(response)
+        })
 
     //     fetch('/add-program', {
     //         method: 'POST',
@@ -37,20 +56,21 @@ class ProgramForm extends React.Component {
 
     render() {
         return (
+        <div>
+            {this.props.children}
         <form onSubmit={this.handleSubmit}>
-        
             <label>Program:</label>
-                <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+                <input type="text" value={this.state.program} onChange={this.handleChangeProgram}></input>
             <label>College:</label>
-                <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+                <input type="text" value={this.state.college} onChange={this.handleChangeCollege}></input>
             <label>Cohort</label>
-                <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+                <input type="text" value={this.state.cohort} onChange={this.handleChangeCohort}></input>
             <label>Link:</label>
-                <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+                <input type="text" value={this.state.link} onChange={this.handleChangeLink}></input>
             <input type="submit" value="Submit"/>  
-            <button onClick= { () => this.props.onDelete(this.props.program.id)}>Delete</button>
 	    </form>
-        
+        <button onClick= {() => this.props.onDelete(this.props.form.id)}>Delete</button>
+        </div>
         );
     }
 }
@@ -75,22 +95,23 @@ class ProgramFormContainer extends React.Component {
         const program = this.state.listOfForms.concat(<ProgramForm />);
         this.setState({listOfForms : program});
     }
-    // render(){
-    //   return (
-    //     <div>
-    //         <p>Let's add some programs.</p>
-    //         <button id="program" type="button" onClick={this.addProgram}>Add Program</button>
 
-    //         {this.state.listOfForms.map(item => ( 
-    //             <div key={item}>{item}</div>)
-    //         )}
-            
-  
-    //     </div>
+    handleDelete = formId => {
+        const forms = this.state.forms.filter(f => f.id !== formId);
+        this.setState({ forms }); 
+    }; 
+
     render() {
-        return (
+        return ( 
         <div>
-            {this.state.forms.map(form => <ProgramForm key={form.id} />)}
+            {this.state.forms.map(form => (
+                <ProgramForm 
+                    key={form.id} 
+                    onDelete={this.handleDelete} 
+                    form={form}
+                    ><h4>Program#{form.id}</h4>
+                </ProgramForm>
+             ))}
         </div>
         );
     }
